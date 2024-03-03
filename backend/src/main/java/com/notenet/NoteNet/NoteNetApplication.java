8d6d53c744fd228d4a79014cd47e20e9c293b79d
Sanorian <krajnov.sascha@yandex.ru>
2024-03-03 15:19:24 +0400

    Загрузил базовую версию Spring Boot сервера

---
+++ backend/src/main/java/com/notenet/NoteNet/NoteNetApplication.java
@@ -0,0 +1,18 @@
+package com.notenet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class NoteNetApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NoteNetApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ backend/src/test/java/com/notenet/NoteNet/NoteNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notenet.NoteNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
