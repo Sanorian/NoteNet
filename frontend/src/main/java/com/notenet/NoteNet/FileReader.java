5f3ebe0bc6a3cbd0171bfeee4a30702b9c0d0ca2
Sanorian <krajnov.sascha@yandex.ru>
2024-03-18 14:52:56 +0400

    Подготовил шаблоны для backend-а web и application серверов

---
+++ frontend/src/main/java/com/notenet/NoteNet/FileReader.java
@@ -0,0 +1,13 @@
+package com.notenet;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Scanner;
+public class FileReader {
+    public static String readFile(String path) throws IOException {
+        Scanner scanner = new Scanner(Paths.get(path), StandardCharsets.UTF_8);
+        String content = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        return content;
+    }
+}
---
+++ frontend/src/main/java/com/notenet/NoteNet/NoteNetApplication.java
@@ -0,0 +1,35 @@
+package com.notenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.IOException;
+
+@SpringBootApplication
+@Controller
+public class NoteNetApplication {
+	static String index;
+	static String registration;
+
+	public static void main(String[] args) throws IOException {
+		index = com.notenet.FileReader.readFile("pages/index.html");
+		SpringApplication.run(NoteNetApplication.class, args);
+	}
+
+	@GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	public String index() {
+		return index;
+	}
+	@GetMapping(value = "/registration", produces = MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	public String registration() {
+		return registration;
+	}
+}
