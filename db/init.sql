0d2cddef6bcbf5aceb3dc26c5b9d1d8e8e188642
Sanorian <krajnov.sascha@yandex.ru>
2024-03-24 21:37:51 +0400

    Доописал бэкенд. Баги буду фиксить потом

--- db/init.sql
+++ db/init.sql
@@ -4,5 +4,6 @@ CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 
 CREATE TABLE notes (id INT PRIMARY KEY AUTO_INCREMENT,
                     user_id INT REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
+                    type VARCHAR(8)
                     note_text TEXT,
                     update_time DATETIME);
